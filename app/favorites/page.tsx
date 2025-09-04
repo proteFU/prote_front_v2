dae0b9dd637142a3ce5862fe3865b6a6424e09b6
dpqlsn <a01037111209@gmail.com>
2025-09-04 23:37:45 +0900

    feat(#7) :: 음악 재생

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,10 +1,15 @@
 "use client"
 
+import { useRef } from "react"
+import { usePlayer } from "@/contexts/PlayerContext"
 import PlayList from "@/components/PlayList"
 import MyMusic from "@/components/MyMusic"
 import SectionTitle from "@/components/SectionTitle"
 
 export default function Favorites() {
+  const { playTrack } = usePlayer()
+  const audioRef = useRef<HTMLAudioElement>(null)
+
   const playlists = [
     { image: "/playlist/playlist1.svg", title: "My Happy Playlist", subtitle: "Made by Guest User" },
     { image: "/playlist/playlist2.svg", title: "Chill Vibes", subtitle: "Made by Guest User" },
@@ -12,13 +17,30 @@ export default function Favorites() {
   ]
 
   const myMusics = [
-    { image: "/song/song1.svg", title: "Blinding Lights", subtitle: "The Weeknd" },
-    { image: "/song/song2.svg", title: "Watermelon Sugar", subtitle: "Harry Styles" },
-    { image: "/song/song3.svg", title: "Levitating", subtitle: "Dua Lipa" },
-    { image: "/song/song4.svg", title: "Good 4 U", subtitle: "Olivia Rodrigo" },
-    { image: "/song/song5.svg", title: "Stay", subtitle: "The Kid LAROI & Justin Bieber" },
-    { image: "/song/song6.svg", title: "Industry Baby", subtitle: "Lil Nas X" },
+    { image: "/song/song1.svg", title: "Blinding Lights", subtitle: "The Weeknd", src: "/songs/song1.mp3", id: "1" },
+    { image: "/song/song2.svg", title: "Watermelon Sugar", subtitle: "Harry Styles", src: "/songs/song2.mp3", id: "2" },
+    { image: "/song/song3.svg", title: "Levitating", subtitle: "Dua Lipa", src: "/songs/song3.mp3", id: "3" },
+    { image: "/song/song4.svg", title: "Good 4 U", subtitle: "Olivia Rodrigo", src: "/songs/song4.mp3", id: "4" },
+    { image: "/song/song5.svg", title: "Stay", subtitle: "The Kid LAROI & Justin Bieber", src: "/songs/song5.mp3", id: "5" },
+    { image: "/song/song6.svg", title: "Industry Baby", subtitle: "Lil Nas X", src: "/songs/song6.mp3", id: "6" },
   ]
+
+  const handlePlay = (music: typeof myMusics[0]) => {
+    playTrack(
+      {
+        id: music.id,
+        name: music.title,
+        artist: music.subtitle,
+        image: music.image,
+      },
+      true
+    )
+
+    if (audioRef.current) {
+      audioRef.current.src = music.src
+      audioRef.current.play()
+    }
+  }
 
   return (
     <div className="flex flex-col gap-5">
@@ -28,12 +50,16 @@ export default function Favorites() {
           <PlayList key={idx} image={pl.image} title={pl.title} subtitle={pl.subtitle} />
         ))}
       </div>
+
       <SectionTitle title="My Music Recap" />
       <div className="flex flex-col gap-y-4 pl-4 pr-4">
-        {myMusics.map((music, idx) => (
-          <MyMusic key={idx} image={music.image} title={music.title} subtitle={music.subtitle} />
+        {myMusics.map((music) => (
+          <div key={music.id} onClick={() => handlePlay(music)} className="cursor-pointer">
+            <MyMusic image={music.image} title={music.title} subtitle={music.subtitle} />
+          </div>
         ))}
       </div>
+      <audio ref={audioRef} />
     </div>
   )
 }
